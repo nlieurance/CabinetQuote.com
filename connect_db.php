5ed66561bdb2111340b9608027cdc27fa43b3516
Nick Lieurance <nlieurance@yahoo.com>
2025-12-23 19:08:07 -0700

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,13 +1,12 @@
 <?php
-$dbc = mysqli_connect(
-  getenv('DB_HOST'),
-  getenv('DB_PORT'),
-  getenv('DB_USER'),
-  getenv('DB_PASS'),
-  getenv('DB_NAME')
-);
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = (int) getenv('DB_PORT'); 
+
+$dbc = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$dbc) {
-  die('Database connection failed: ' . mysqli_connect_error());
+    die('Database connection failed: ' . mysqli_connect_error());
 }
-?>
