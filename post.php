c45641f098fe1c62f728ed1e9b7fe6111c1e0f9e
Nick Lieurance <nlieurance@yahoo.com>
2026-01-02 09:06:43 -0700

    Updated includes code in post.php

--- post.php
+++ post.php
@@ -1,11 +1,11 @@
 <?php
+if (!isset($_SESSION['id'])){
+	header('location:login.php');
+}
 include('header.php'); 
 //require('../connect_db.php')
 if (!isset($_SESSION)){
 	session_start();
-}
-if (!isset($_SESSION['id'])){
-	header('location:login.php');
 }
 if ($_SESSION['role'] == 'seller'){
 	echo 'Oops! You\'re logged in as a cabinetmaker. That means you can provide quotes, but you can\'t post projects.';
